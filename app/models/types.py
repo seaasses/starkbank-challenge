f0808a2b68570e5886f503010d14ebc8084c2a45
seaasses <marcusvglac@gmail.com>
2025-02-14 17:51:14 -0300

    add StarkBankInvoiceEvent to types

--- app/models/types.py
+++ app/models/types.py
@@ -3,7 +3,7 @@ from enum import Enum
 from pydantic import Field, field_validator
 import re
 from datetime import date
-from typing import Optional
+from typing import Optional, Literal
 
 
 class Person(BaseModel):
@@ -44,3 +44,53 @@ class Invoice(BaseModel):
     amount: int = Field(gt=0)
     person: Person
     due_date: Optional[date] = None
+
+
+class StarkBankInvoice(BaseModel):
+    amount: int
+    brcode: str
+    created: str
+    descriptions: list[str]
+    discountAmount: int
+    discounts: list[str]
+    displayDescription: str
+    due: str
+    expiration: int
+    fee: int
+    fine: float
+    fineAmount: float
+    id: str
+    interest: float
+    interestAmount: float
+    link: str
+    name: str
+    nominalAmount: int
+    pdf: str
+    rules: list[str]
+    splits: list[str]
+    status: str
+    tags: list[str]
+    taxId: str
+    transactionIds: list[str]
+    updated: str
+
+
+class StarkBankInvoiceEventLog(BaseModel):
+    authentication: str
+    created: str
+    errors: list[str]
+    id: str
+    invoice: StarkBankInvoice
+    type: Literal["created", "credited", "paid"]  # TODO see in the docs all the types
+
+
+class _StarkBankInvoiceEvent(BaseModel):
+    created: str
+    id: str
+    log: StarkBankInvoiceEventLog
+    subscription: Literal["invoice"]
+    workspaceId: str
+
+
+class StarkBankInvoiceEvent(BaseModel):
+    event: _StarkBankInvoiceEvent
