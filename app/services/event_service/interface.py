5b7d69de8968150dbd069deaf379575d1bd59552
seaasses <marcusvglac@gmail.com>
2025-02-15 13:29:50 -0300

    implement StarkBank event fetcher and status changer

--- app/services/event_service/interface.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-from app.models.types import StarkBankEvent
-
-
-class EventFetcher(ABC):
-    @abstractmethod
-    def fetch_undelivered_events(self) -> list[StarkBankEvent]:
-        pass
-
