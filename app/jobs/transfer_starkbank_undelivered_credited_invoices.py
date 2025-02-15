bd2395a400f59a1ffffc0464fed4ff6edcabd88b
seaasses <marcusvglac@gmail.com>
2025-02-15 13:53:36 -0300

    add job to transfer undelivered events

---
+++ app/jobs/transfer_starkbank_undelivered_credited_invoices.py
@@ -0,0 +1,25 @@
+from app.services.starkbank_event_services.implementation import (
+    StarkBankEventFetcher,
+    StarkBankEventStatusChanger,
+)
+from app.services.transfer_service.implementation import StarkBankTransferSender
+from app.models.types import Transfer
+from app.core.config import settings
+
+
+def transfer_starkbank_undelivered_credited_invoices():
+    event_fetcher = StarkBankEventFetcher(settings.starkbank_project)
+    event_status_changer = StarkBankEventStatusChanger(settings.starkbank_project)
+    transfer_sender = StarkBankTransferSender(settings.starkbank_project)
+    for event in event_fetcher.fetch_undelivered_events():
+        if event.subscription == "invoice" and event.log["type"] == "credited":
+            transfer_amount = (
+                event.log["invoice"]["amount"] - event.log["invoice"]["fee"]
+            )
+            # TODO: batch transfers
+            transfer = Transfer(
+                account=settings.default_account,
+                amount=transfer_amount,
+            )
+            transfer_sender.send(transfer)
+            event_status_changer.mark_as_delivered(event.id)
