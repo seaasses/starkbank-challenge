7fbabbe20cb769ff2333a573b4968656bb6f81e8
seaasses <marcusvglac@gmail.com>
2025-02-16 20:52:18 -0300

    fix transfer starkbank undelivered credited invoices to pass tests

--- app/jobs/transfer_starkbank_undelivered_credited_invoices.py
+++ app/jobs/transfer_starkbank_undelivered_credited_invoices.py
@@ -12,14 +12,19 @@ def transfer_starkbank_undelivered_credited_invoices():
     event_status_changer = StarkBankEventStatusChanger(settings.starkbank_project)
     transfer_sender = StarkBankTransferSender(settings.starkbank_project)
     for event in event_fetcher.fetch_undelivered_events():
-        if event.subscription == "invoice" and event.log["type"] == "credited":
-            transfer_amount = (
-                event.log["invoice"]["amount"] - event.log["invoice"]["fee"]
-            )
-            # TODO: batch transfers
-            transfer = Transfer(
-                account=settings.default_account,
-                amount=transfer_amount,
-            )
-            transfer_sender.send(transfer)
+        try:
+            if event.subscription == "invoice" and event.log["type"] == "credited":
+                transfer_amount = (
+                    event.log["invoice"]["amount"] - event.log["invoice"]["fee"]
+                )
+                # TODO: batch transfers
+                transfer = Transfer(
+                    account=settings.default_account,
+                    amount=transfer_amount,
+                )
+                transfer_sender.send(transfer)
+            # Only mark as delivered if no exception occurred
             event_status_changer.mark_as_delivered(event.id)
+        except Exception:
+            # Skip failed events and continue with the next one
+            pass
