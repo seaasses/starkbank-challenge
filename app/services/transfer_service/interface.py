c38265b65e65dc43df6a5b1f10f98c63d0abf60e
seaasses <marcusvglac@gmail.com>
2025-02-17 16:33:14 -0300

    send api tests to the api folder

--- app/services/transfer_service/interface.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-from app.models.types import Transfer
-
-
-class TransferSender(ABC):
-    @abstractmethod
-    def send(self, transfer: Transfer) -> None:
-        pass 
