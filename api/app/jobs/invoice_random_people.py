a44cd03863ca5a07c8cf38139a2ba13e71a5b0e6
seaasses <marcusvglac@gmail.com>
2025-02-18 16:30:48 -0300

    fix invoice random people to use queue

--- api/app/jobs/invoice_random_people.py
+++ api/app/jobs/invoice_random_people.py
@@ -1,8 +1,10 @@
 from app.core.config import settings
 from app.models.types import Invoice
-from app.services.invoice_service.implementation import StarkBankInvoiceSender
+from app.services.invoice_service.implementation import QueueInvoiceSender
 from app.services.random_person_getter.implementation import RandomPersonGetter
+from app.services.queue_service.implementation import RabbitMQService
 import random
+import os
 
 
 def invoice_random_people(n_min: int, n_max: int):
@@ -10,11 +12,20 @@ def invoice_random_people(n_min: int, n_max: int):
         raise ValueError("n_min and n_max must be non-negative")
     if n_min > n_max:
         raise ValueError("n_min cannot be greater than n_max")
-    
+
     n = random.randint(n_min, n_max)
     invoices = []
-    invoice_sender = StarkBankInvoiceSender(settings.starkbank_project)
+
+    queue_service = RabbitMQService(
+        queue_name="task_queue",
+        rabbitmq_host=os.getenv("RABBITMQ_HOST"),
+        rabbitmq_port=os.getenv("RABBITMQ_PORT"),
+        rabbitmq_user=os.getenv("RABBITMQ_USER"),
+        rabbitmq_pass=os.getenv("RABBITMQ_PASS"),
+    )
+    invoice_sender = QueueInvoiceSender(queue_service)
     person_getter = RandomPersonGetter()
+
     for _ in range(n):
         person = person_getter.get_random_person()
         amount = random.randint(100, 10000000000 - 1)
