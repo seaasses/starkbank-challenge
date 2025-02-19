677d2d1d5cda42990f2617558fea0daf49b49358
seaasses <marcusvglac@gmail.com>
2025-02-18 23:50:48 -0300

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,26 +9,34 @@ APP_NAME="starkbank-challenge"
 echo "Getting AWS account ID..."
 AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
 
-echo "Constructing ECR repository URL..."
-ECR_REPO="${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${APP_NAME}"
+echo "Constructing ECR repository URLs..."
+API_ECR_REPO="${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${APP_NAME}-api"
+QUEUE_ECR_REPO="${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${APP_NAME}-queue"
 
 echo "Logging into ECR..."
 aws ecr get-login-password --region ${AWS_REGION} | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com
 
-echo "Building Docker image..."
-docker build -t ${APP_NAME}:latest ./api/
+echo "Building and pushing API Docker image..."
+docker build -t ${APP_NAME}-api:latest ./api/
+docker tag ${APP_NAME}-api:latest ${API_ECR_REPO}:latest
+docker push ${API_ECR_REPO}:latest
 
-echo "Tagging Docker image..."
-docker tag ${APP_NAME}:latest ${ECR_REPO}:latest
+echo "Building and pushing Queue Consumer Docker image..."
+docker build -t ${APP_NAME}-queue:latest ./queue/
+docker tag ${APP_NAME}-queue:latest ${QUEUE_ECR_REPO}:latest
+docker push ${QUEUE_ECR_REPO}:latest
 
-echo "Pushing image to ECR..."
-docker push ${ECR_REPO}:latest
+echo "Images pushed successfully!"
 
-echo "Image pushed successfully!" 
-
-echo "Forcing new deployment of ECS service..."
+echo "Forcing new deployment of ECS services..."
 aws ecs update-service --cluster ${APP_NAME}-cluster \
     --service ${APP_NAME}-service \
+    --force-new-deployment \
+    --region ${AWS_REGION} \
+    --no-cli-pager > /dev/null 2>&1 &
+
+aws ecs update-service --cluster ${APP_NAME}-cluster \
+    --service ${APP_NAME}-queue-consumer \
     --force-new-deployment \
     --region ${AWS_REGION} \
     --no-cli-pager > /dev/null 2>&1 &
