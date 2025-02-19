7a5615890a2b8079f5af6331ba902b3f6dc556da
seaasses <marcusvglac@gmail.com>
2025-02-19 00:01:00 -0300

    add test method to queue service

--- api/app/services/queue_service/implementation.py
+++ api/app/services/queue_service/implementation.py
@@ -1,6 +1,7 @@
 import pika
 import json
 from app.services.queue_service.interface import QueueService
+import socket
 
 
 class RabbitMQService(QueueService):
@@ -23,22 +24,31 @@ class RabbitMQService(QueueService):
 
     def __connect(self):
         try:
-            self.connection = pika.BlockingConnection(
-                pika.ConnectionParameters(
-                    host=self.rabbitmq_host,
-                    port=self.rabbitmq_port,
-                    credentials=pika.PlainCredentials(
-                        self.rabbitmq_user, self.rabbitmq_pass
-                    ),
-                )
+
+            # Try to resolve hostname
+            try:
+                socket.gethostbyname(self.rabbitmq_host)
+            except socket.gaierror as e:
+                raise
+
+            credentials = pika.PlainCredentials(self.rabbitmq_user, self.rabbitmq_pass)
+
+            parameters = pika.ConnectionParameters(
+                host=self.rabbitmq_host,
+                port=self.rabbitmq_port,
+                credentials=credentials,
+                connection_attempts=3,
+                retry_delay=5,
+                socket_timeout=5,
             )
+
+            self.connection = pika.BlockingConnection(parameters)
+
             self.channel = self.connection.channel()
 
             self.channel.queue_declare(queue=self.queue_name, durable=True)
-            print(f"Connected to RabbitMQ and declared queue: {self.queue_name}")
 
         except Exception as e:
-            print(f"Error connecting to RabbitMQ: {str(e)}")
             raise
 
     def publish_message(self, message: dict[str, str]) -> bool:
@@ -57,11 +67,9 @@ class RabbitMQService(QueueService):
                 ),
             )
 
-            print(f"Published message: {message}")
             return True
 
         except Exception as e:
-            print(f"Error publishing message: {str(e)}")
             return False
         finally:
             if self.connection and not self.connection.is_closed:
@@ -69,3 +77,13 @@ class RabbitMQService(QueueService):
 
     def publish_messages(self, messages: list[dict[str, str]]) -> list[bool]:
         return [self.publish_message(message) for message in messages]
+
+    def test_connection(self) -> bool:
+        try:
+            self.__connect()
+            return True
+        except Exception as e:
+            return False
+        finally:
+            if self.connection and not self.connection.is_closed:
+                self.connection.close()
